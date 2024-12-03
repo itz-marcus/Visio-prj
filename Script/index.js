1df0ff7c6ba01a100404fed321860f4b54fdd791
itz_marcus <marcusadonis5@gmail.com>
2024-12-03 14:39:00 +0200

    update

--- Script/index.js
+++ Script/index.js
@@ -10,3 +10,12 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+function myFunction() {
+  var x = document.getElementById("body-cover");
+  if (x.classList.contains("hidden")) {
+    x.classList.remove("hidden");
+  } else {
+    x.classList.add("hidden");
+  }
+}
