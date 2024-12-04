fa79afb2a2dd4aa3cdd211f9af45ddd35b703153
itz_marcus <marcusadonis5@gmail.com>
2024-12-04 16:31:03 +0200

    update

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,4 @@
+/*------------------------------burger------------------------------ */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -11,6 +12,7 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   navMenu.classList.remove("active");
 }))
 
+/*------------------------------background darken------------------------------ */
 function myFunction() {
   var x = document.getElementById("body-cover");
   if (x.classList.contains("hidden")) {
@@ -18,4 +20,39 @@ function myFunction() {
   } else {
     x.classList.add("hidden");
   }
+}
+
+/*------------------------------slideshow------------------------------ */
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  
+  // Hide all slides
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  
+  // Remove active class from all dots
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  // Display the current slide
+  x[slideIndex[no]-1].style.display = "block";  
+  
+  // Add active class to the current dot
+  dots[slideIndex[no]-1].className += " active";
 }
